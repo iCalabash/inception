e59e8fdabab631870fabb9e3b387c5f1721e7176
iCalabash <136085453@qq.com>
2016-12-06 17:22:07 +0800

    Initial Commit

---
+++ inception/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  inception
+//
+//  Created by Faber on 2016/12/6.
+//  Copyright © 2016年 Faber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
